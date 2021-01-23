8cc47343f2e529ce346e86c50b08d88bd38b250e
AmericanWonton <jbkeller0303@gmail.com>
2021-01-22 20:23:43 -0600

    Added a large amount of starter files

---
+++ Makefile
@@ -0,0 +1,29 @@
+run:
+	go run *.go
+gomod-exp:
+	export GO111MODULE=on
+gobuild:
+	GOOS=linux GOARCH=amd64 go build -o mainwebpage
+dockerbuild:
+	docker build -t mainwebpage .
+dockerbuildandpush:
+	docker build -t mainwebpage .
+	docker tag mainwebpage americanwonton/mainwebpage
+	docker push americanwonton/mainwebpage
+dockerrun:
+	docker run -it -p 80:80 mainwebpage
+dockerrundetached:
+	docker run -d -p 80:80 mainwebpage
+dockerrunitvolume:
+	docker run -it -p 80:80 -v photo-images:/static/images mainwebpage
+dockerrundetvolume:
+	docker run -d -p 80:80 -v photo-images:/static/images mainwebpage
+dockertagimage:
+	docker tag mainwebpage americanwonton/mainwebpage
+dockerimagepush:
+	docker push americanwonton/mainwebpage
+dockerallpush:
+	docker tag mainwebpage americanwonton/mainwebpage
+	docker push americanwonton/mainwebpage
+dockerseeshell:
+	docker run -it mainwebpage sh
---
+++ main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+/* TEMPLATE DEFINITION BEGINNING */
+var template1 *template.Template
+
+//Define function maps
+var funcMap = template.FuncMap{
+	"upperCase": strings.ToUpper, //upperCase is a key we can call inside of the template html file
+}
+
+//DEBUG not sure if needed
+type MessageViewData struct {
+	TestString  string    `json:"TestString"`
+	TheMessages []Message `json:"TheMessages"`
+	WhatPage    int       `json:"WhatPage"`
+}
+
+//DEBUG  Message displayed on the board
+type Message struct {
+	MessageID       int       `json:"MessageID"`       //ID of this Message
+	UserID          int       `json:"UserID"`          //ID of the owner of this message
+	Messages        []Message `json:"Messages"`        //Array of Messages under this one
+	IsChild         bool      `json:"IsChild"`         //Is this message childed to another message
+	HasChildren     bool      `json:"HasChildren"`     //Whether this message has children to list
+	ParentMessageID int       `json:"ParentMessageID"` //The ID of this parent
+	UberParentID    int       `json:"UberParentID"`    //The final parent of this parent, IF EQUAL PARENT
+	Order           int       `json:"Order"`           //Order the commnet is in with it's reply tree
+	RepliesAmount   int       `json:"RepliesAmount"`   //Amount of replies this message has
+	TheMessage      string    `json:"TheMessage"`      //The MEssage in the post
+	DateCreated     string    `json:"DateCreated"`     //When the message was created
+	LastUpdated     string    `json:"LastUpdated"`     //When the message was last updated
+}
+
+/* This is the current amount of results our User is looking at
+it changes as the User clicks forwards or backwards for more results */
+var currentPageNumber int = 1
+
+//Parse our templates
+func init() {
+	/* Assign blank value to map so no nil errors occur */
+	template1 = template.Must(template.ParseGlob("./static/templates/*"))
+}
+
+//Writes to the log; called from most anywhere in this program!
+func logWriter(logMessage string) {
+	//Logging info
+
+	wd, _ := os.Getwd()
+	logDir := filepath.Join(wd, "logging", "logging.txt")
+	logFile, err := os.OpenFile(logDir, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+
+	defer logFile.Close()
+
+	if err != nil {
+		fmt.Println("Failed opening log file")
+	}
+
+	log.SetOutput(logFile)
+
+	log.Println(logMessage)
+}
+
+//Handles the Index requests
+func index(w http.ResponseWriter, r *http.Request) {
+
+	userStuff := MessageViewData{
+		TestString:  "bootyhole",
+		TheMessages: []Message{},
+		WhatPage:    currentPageNumber,
+	}
+
+	/* Execute template, handle error */
+	err1 := template1.ExecuteTemplate(w, "index.gohtml", userStuff)
+	HandleError(w, err1)
+}
+
+//Handles the test page
+func test(w http.ResponseWriter, r *http.Request) {
+	userStuff := MessageViewData{
+		TestString:  "bootyhole",
+		TheMessages: []Message{},
+		WhatPage:    currentPageNumber,
+	}
+
+	/* Execute template, handle error */
+	err1 := template1.ExecuteTemplate(w, "test.gohtml", userStuff)
+	HandleError(w, err1)
+}
+
+// Handle Errors passing templates
+func HandleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
+}
+
+//Handles all requests coming in
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	//Write to logger that we are handling requests
+	debugMessage := "\n\nWe are now handling requests"
+	fmt.Println(debugMessage)
+	logWriter(debugMessage)
+
+	http.Handle("/favicon.ico", http.NotFoundHandler()) //For missing FavIcon
+	myRouter.HandleFunc("/", index)
+	myRouter.HandleFunc("/test", test)
+	//Mongo No-SQL Stuff
+
+	//Serve our static files
+	myRouter.Handle("/", http.FileServer(http.Dir("./static")))
+	myRouter.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	log.Fatal(http.ListenAndServe(":80", myRouter))
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano()) //Randomly Seed
+
+	//Handle Requests
+	handleRequests()
+}
